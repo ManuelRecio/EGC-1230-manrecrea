f9a9025d7437d46f74e30ac05af833b1389df081
ManuelRecio <man.recio.real@gmail.com>
2021-02-12 10:40:08 +0100

    arreglos de decide

---
+++ examen/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/manu/Escritorio/REpo_Examen/EGC-1230-manrecrea/examen/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
